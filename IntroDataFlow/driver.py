5e69f2cc2a7aa96674c97ec1a2b5ec17217943a0
Henrique Furst <henrique.furst@softruck.com>
2023-09-17 22:23:37 -0300

    feat: simple driver.py test for stdin programs

--- IntroDataFlow/driver.py
+++ IntroDataFlow/driver.py
@@ -1,30 +1,15 @@
 import sys
-
-import lang
 import todo
+import parser
+import staticanalysis as sa
 
-def test(case, args):
-    if case == 0:
-        print(VPL_mSort.ll2py(VPL_mSort.py2ll(args)))
-    elif case == 1:
-        print(VPL_mSort.size(VPL_mSort.py2ll(args)))
-    elif case == 2:
-        print(VPL_mSort.sorted(VPL_mSort.py2ll(args)))
-    elif case == 3:
-        print(VPL_mSort.sorted(VPL_mSort.py2ll(args)))
-    elif case == 4:
-        print(VPL_mSort.sum(VPL_mSort.py2ll(args)))
-    elif case == 5:
-        print(VPL_mSort.ll2py(VPL_mSort.mSort(VPL_mSort.py2ll(args))))
-    elif case == 6:
-        print(VPL_mSort.max(VPL_mSort.py2ll(args)))
-    elif case == 7:
-        print(VPL_mSort.get(VPL_mSort.py2ll(args[1:]), args[0]))
-    else:
-        print("Unknown case: ", case)
 
-for line in sys.stdin:
-    inps = [int(x) for x in list(line.split(" "))]
-    case = inps[0]
-    args = inps[1:]
-    test(case, args)
+def run_test():
+    lines = sys.stdin.readlines()
+    program, _ = parser.build_cfg(lines)
+    result = sa.run_analysis_on_program(program, todo.ReachingDefinitions)
+    result.print()
+
+
+if __name__ == "__main__":
+    run_test()
