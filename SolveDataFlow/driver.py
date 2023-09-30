b7272e12029fcea5dbe19ac246f07d99c0b71498
Henrique Furst <henrique.furst@softruck.com>
2023-09-30 15:52:17 -0300

    chore: updated driver to use worklist file

--- SolveDataFlow/driver.py
+++ SolveDataFlow/driver.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 import sys
-import todo
 import parser
 import static_analysis as sa
+import worklist
 
 
 def run_test():
     lines = sys.stdin.readlines()
     program, _ = parser.build_cfg(lines)
-    result = sa.run_analysis_on_program(program, todo.ReachingDefinitions)
-    result.print()
+    cs = sa.Liveness.build_constraints(program)
+    env = sa.Liveness.build_constraint_env(program)
+    sol = worklist.solve_worklist(cs, env)
+    sol.print()
 
 
 if __name__ == "__main__":
