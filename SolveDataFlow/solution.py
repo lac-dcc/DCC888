2f64a8ef18035b94498ae21a224392c4b19b2cda
Henrique Furst Scheid <hfscheid@gmail.com>
2023-10-01 21:06:59 -0300

    feat: documented TODO sections

--- SolveDataFlow/solution.py
+++ SolveDataFlow/solution.py
@@ -1,118 +1,48 @@
-import lang
-from parser import build_cfg
-from static_analysis import ConstraintEnv, Constraint, Equation, \
-    StaticAnalysis, chaotic_iterations
-
-from typing import List
+from typing import List, Set
+from static_analysis import ConstraintEnv, Constraint
 
 
-class ReachingDefinitions(StaticAnalysis):
-    """
-    Returns the Reaching Definitions analysis of a program.
-    The definition of a variable reaches a program point if said variable has
-    been defined prior and does not "die" via a new assignment along the way.
+class Worklist:
+    def __init__(s, constraints: List[Constraint]):
+        s.empty = False
+        cIds = [c.id for c in constraints]
+        s.dg = DependenceGraph(cIds)
+        s.build_dependences(constraints)
+        s.worklist = constraints
+        s.csMap = dict()
+        for c in constraints:
+            s.csMap[c.id] = c
 
-    For each instruction
-        p: v = E(s)
+    def build_dependences(s, constraints: List[Constraint]):
+        for c in constraints:
+            uses = c.uses()
+            for var in uses:
+                s.dg.add_dependence(var, c.id)
 
-    The incoming and outgoing reaching definitions are deined as:
-        IN:  Union(OUT(ps), ps in succ(p)
-        OUT: Union(IN(p) - {definitions(v)}, {(p, v)})
+    def insert(s, constraint: Constraint):
+        s.worklist.append(constraint)
 
-    (read more in https://homepages.dcc.ufmg.br/~fernando/classes/dcc888/ementa/slides/IntroDataFlow.pdf)
+    def extract(s) -> Constraint:
+        head = s.worklist.pop()
+        if len(s.worklist) == 0:
+            s.empty = True
+        return head
 
-    Since the program starts with a set environment, definitions from said
-    environment are said to come from instruction -1.
+    def affected_constraints(s, c: Constraint):
+        ids = s.dg.affects(c.id)
+        cs = [s.csMap[id] for id in ids]
+        return cs
 
-    >>> program_lines = [
-    ... '{"a": 1, "b": 2}',
-    ... 'x = add a b',
-    ... 'a = add x a',
-    ... 'b = add a x',
-    ... ]
-    >>> expected_result = ConstraintEnv({
-    ... 'IN_0': {(-1, 'a'), (-1, 'b')},
-    ... 'OUT_0': {(-1, 'a'), (-1, 'b'), (0, 'x')},
-    ... 'IN_1': {(-1, 'a'), (-1, 'b'), (0, 'x')},
-    ... 'OUT_1': {(1, 'a'), (-1, 'b'), (0, 'x')},
-    ... 'IN_2': {(1, 'a'), (-1, 'b'), (0, 'x')},
-    ... 'OUT_2':{(1, 'a'), (1, 'b'), (0, 'x')},
-    ... })
-    >>> program, env = build_cfg(program_lines)
-    >>> result = ReachingDefinitions.run(program)
-    >>> result == expected_result
-    True
-    """
 
-    @classmethod
-    def run(cls, program: List[lang.Inst]) -> ConstraintEnv:
-        constraints = cls.build_constraints(program)
-        env = cls.build_constraint_env(program)
-        env = chaotic_iterations(constraints, env)
-        return env
-
-    @classmethod
-    def build_constraints(cls, program: List[lang.Inst]) -> List[Constraint]:
-        constraints = []
-        for instruction in program:
-            _in = cls.IN(instruction)
-            constraints.append(
-                Constraint(f'IN_{instruction.index}', _in)
-            )
-            _out = cls.OUT(instruction)
-            constraints.append(
-                Constraint(f'OUT_{instruction.index}', _out)
-            )
-        return constraints
-
-    @classmethod
-    def build_constraint_env(cls, program: List[lang.Inst]) -> ConstraintEnv:
-        env = dict()
-        for i in range(len(program)):
-            env[f'IN_{i}'] = set()
-            env[f'OUT_{i}'] = set()
-        return ConstraintEnv(env)
-
-    @classmethod
-    def IN(cls, instruction: lang.Inst) -> Equation:
-        eq = Equation('set', set())
-        for pred in instruction.PREVS:
-            _eq = Equation(eq, 'union', Equation(f'OUT_{pred.index}'))
-            eq = _eq
-        return eq
-
-    @classmethod
-    def OUT(cls, instruction: lang.Inst) -> Equation:
-        _defs = cls.definitions(instruction)
-        defs = []
-        for d in _defs:
-            defs.append((instruction.index, d))
-        _out = Equation(
-            Equation(
-                Equation(f'IN_{instruction.index}'),
-                'minus',
-                cls.definitions_equation(instruction)
-            ),
-            'union',
-            Equation('set', set(defs))
-        )
-        return _out
-
-    @classmethod
-    def definitions_equation(cls, instruction: lang.Inst) -> Equation:
-        v = cls.definitions(instruction)
-        return Equation('set', v)
-
-    @classmethod
-    def definitions(cls, instruction):
-        if type(instruction) == lang.Bt:
-            return set()
-        else:
-            return set([instruction.definition()])
-
-    @classmethod
-    def vars(cls, instruction):
-        if type(instruction) == lang.Bt:
-            return set([instruction.cond])
-        else:
-            return set([*instruction.uses()])
+def solve_worklist(constraints: List[Constraint], _env: ConstraintEnv) \
+        -> ConstraintEnv:
+    worklist = Worklist(constraints)
+    env = _env.copy()
+    while not worklist.empty:
+        constr = worklist.extract()
+        update = constr.eval(env)
+        if update:
+            affected = worklist.affected_constraints(constr)
+            for c in affected:
+                worklist.insert(c)
+    return env
--- SolveDataFlow/todo.py
+++ SolveDataFlow/todo.py
@@ -1,44 +1,64 @@
-from typing import List, Set
-from static_analysis import ConstraintEnv, Constraint
+from typing import List
+from static_analysis import ConstraintEnv, Constraint, DependenceGraph
 
 
 class Worklist:
-    def __init__(s, constraints: List[Constraint]):
-        s.empty = False
-        cIds = [c.id for c in constraints]
-        s.dg = DependenceGraph(cIds)
-        s.build_dependences(constraints)
-        s.worklist = constraints
-        s.csMap = dict()
-        for c in constraints:
-            s.csMap[c.id] = c
+    """
+    The Worklist is the main data structure used by worklist algorithms.
+    It representes which constraints remain to be solved.
 
-    def build_dependences(s, constraints: List[Constraint]):
-        for c in constraints:
-            uses = c.uses()
-            for var in uses:
-                s.dg.add_dependence(var, c.id)
+    There are two fundamental operations regarding this structure:
+    insert and extract.
+
+    - "insert" adds constraints to the worklist, the result being a new
+    *ordered* worklist. The ordering defines how efficient the worklist
+    algorithm works. For this class insert needs not to return a new
+    object, but keep itself ordered.
+
+    - "extract" pops the first constraint from the ordered worklist.
+
+    TODO: implement the Worklist data structure. You may create any
+    auxiliary methods as necessary, but the current interface must be
+    implemented.
+    """
+
+    def __init__(s, constraints: List[Constraint]):
+        """
+        TODO: given a set of constraints, implement a functioning Worklist.
+        HINT: keep a DependenceGraph object (from static_analysis) to keep
+        track of how constraints affect each other.
+        """
+        pass
 
     def insert(s, constraint: Constraint):
-        s.worklist.append(constraint)
+        """
+        TODO: add new constraints to the active worklist.
+        """
+        pass
 
     def extract(s) -> Constraint:
-        head = s.worklist.pop()
-        if len(s.worklist) == 0:
-            s.empty = True
-        return head
+        """
+        TODO: pop the fist constraint in the ordered worklist.
+        """
 
     def affected_constraints(s, c: Constraint):
-        ids = s.dg.affects(c.id)
-        cs = [s.csMap[id] for id in ids]
-        return cs
+        """
+        TODO: return a collection of the Constraints that are affected by 'c'.
+        """
+        pass
+
+    def empty(s) -> bool:
+        """
+        TODO: let the caller know if the implemented worklist is empty
+        """
+        pass
 
 
 def solve_worklist(constraints: List[Constraint], _env: ConstraintEnv) \
         -> ConstraintEnv:
     worklist = Worklist(constraints)
     env = _env.copy()
-    while not worklist.empty:
+    while not worklist.empty():
         constr = worklist.extract()
         update = constr.eval(env)
         if update:
